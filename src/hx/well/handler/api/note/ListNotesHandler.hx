0e587c296c300a7da82c1fa9c7d3d3d5abbb6e32
barisyild <fb_bcy@hotmail.com>
2025-09-06 01:38:07 +0300

    Add debug logging for user ID in ListNotesHandler under hashlink target

--- src/hx/well/handler/api/note/ListNotesHandler.hx
+++ src/hx/well/handler/api/note/ListNotesHandler.hx
@@ -10,6 +10,9 @@ import hx.well.model.Note;
 class ListNotesHandler extends AbstractHandler {
     public function execute(request:Request):AbstractResponse {
         var user:User = auth().user();
+        #if hl
+        trace("user_id", user.id); // user id sometimes returns different value in hashlink shit
+        #end
         return Note.instance.where("user_id", user.id).get();
     }
 }
