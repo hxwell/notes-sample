caa9834eb277c6096fd3b0eb00eafe511862231f
barisyild <fb_bcy@hotmail.com>
2025-09-06 00:44:20 +0300

    logout implemented

---
+++ src/hx/well/handler/api/LogoutHandler.hx
@@ -0,0 +1,14 @@
+package hx.well.handler.api;
+
+import hx.well.handler.AbstractHandler;
+import hx.well.http.AbstractResponse;
+import hx.well.http.Request;
+import hx.well.http.RequestStatic.auth;
+import hx.well.http.ResponseBuilder;
+
+class LogoutHandler extends AbstractHandler {
+    public function execute(request:Request):AbstractResponse {
+        auth().logout();
+        return ResponseBuilder.asRedirectRoute("login");
+    }
+}
